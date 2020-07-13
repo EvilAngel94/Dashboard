a6b887fe6d0e60999fe1d2aefb1721826f9db924
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-07-13 15:58:51 +0200

    WeatherForecast - Very basics of JS are implemented.
    Still need some prettier layout and so on. But the basics are there.

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -3,4 +3,45 @@
  */
 function getWeatherForecast() {
 
+    fetch('http://localhost:8080/rest/weather')
+        .then(response => {
+            return response.json()
+        }).then(
+        weatherForecast => {
+            console.log(weatherForecast)
+
+            const weatherRow = document.getElementById('row-details')
+            weatherRow.className = 'row-detail'
+
+            const infoBox = document.createElement('div')
+            infoBox.className = 'info-box'
+            weatherRow.append(infoBox)
+
+            const img = document.createElement('img')
+            img.src = weatherForecast.homeWeatherPicture
+            img.className = 'info-box-img'
+            infoBox.append(img)
+
+            const paragraph = document.createElement('p')
+            paragraph.className = 'info-box-p'
+            const textToDisplay = weatherForecast.weatherForecast.homeLocation + " /nTemerature feel: " + weatherForecast.weatherForecast.homeTemperatureFeelsLike + "/nThe actual temp is: " + weatherForecast.weatherForecast.homeTemperature
+            paragraph.textContent = textToDisplay
+            infoBox.append(paragraph)
+
+            const infoBox2 = document.createElement('div')
+            infoBox2.className = 'info-box'
+            weatherRow.append(infoBox2)
+
+            const imgWork = document.createElement('img')
+            imgWork.src = weatherForecast.workWeatherPicture
+            imgWork.className = 'info-box-img'
+            infoBox2.append(imgWork)
+
+        }
+    ).catch(error => {
+        console.log(error)
+        console.log('Something somewhere went wrong. What could it be..?')
+    })
+
 }
+
