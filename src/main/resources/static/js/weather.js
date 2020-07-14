77f05d11ed2ceed6e8a7e286680f335de57b032d
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-07-14 16:39:55 +0200

    WeatherForecast - JS function is working, and only possible to click once

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -1,47 +1,74 @@
+var isAlreadyExecuted = false
+
 /**
  * This function is responsible for displaying the information obtained from the server regarding weather forecast
  */
 function getWeatherForecast() {
+    if (!isAlreadyExecuted) {
+        fetch('http://localhost:8080/rest/weather')
+            .then(response => {
+                return response.json()
+            }).then(
+            weatherForecast => {
+                const weatherRow = document.getElementById('row-details')
+                weatherRow.className = 'row-detail'
 
-    fetch('http://localhost:8080/rest/weather')
-        .then(response => {
-            return response.json()
-        }).then(
-        weatherForecast => {
-            console.log(weatherForecast)
+                const forecast = weatherForecast.weatherForecast;
 
-            const weatherRow = document.getElementById('row-details')
-            weatherRow.className = 'row-detail'
+                // Creation of the home element
+                const homePictureLocation = weatherForecast.homeWeatherPicture
+                const location = forecast.homeLocation
+                const temperature = forecast.homeTemperature
+                const feelsLikeTemp = forecast.homeTemperatureFeelsLike
+                createInfoBox(weatherRow, homePictureLocation, location, temperature, feelsLikeTemp);
 
-            const infoBox = document.createElement('div')
-            infoBox.className = 'info-box'
-            weatherRow.append(infoBox)
+                // Creation of the work element
+                const workPictureLocation = weatherForecast.workWeatherPicture
+                const locationWork = forecast.workLocation
+                const temperatureWork = forecast.workTemperature
+                const temperatureFeelsLikeWork = forecast.workTemperatureFeelsLike
+                createInfoBox(weatherRow, workPictureLocation, locationWork, temperatureWork, temperatureFeelsLikeWork)
+                isAlreadyExecuted = true
+            }
+        ).catch(error => {
+            console.log(error)
+            console.log('Something somewhere went wrong. What could it be...?')
+        })
+    }
+}
 
-            const img = document.createElement('img')
-            img.src = weatherForecast.homeWeatherPicture
-            img.className = 'info-box-img'
-            infoBox.append(img)
+function createInfoBox(weatherRow, weatherPicture, location, temperature, feelsLikeTemp) {
+    const infoBox = document.createElement('div')
+    infoBox.className = 'info-box'
+    weatherRow.append(infoBox)
 
-            const paragraph = document.createElement('p')
-            paragraph.className = 'info-box-p'
-            const textToDisplay = weatherForecast.weatherForecast.homeLocation + " /nTemerature feel: " + weatherForecast.weatherForecast.homeTemperatureFeelsLike + "/nThe actual temp is: " + weatherForecast.weatherForecast.homeTemperature
-            paragraph.textContent = textToDisplay
-            infoBox.append(paragraph)
+    createImageElement(weatherPicture, infoBox);
+    createInfoBoxElement(location, temperature, feelsLikeTemp, infoBox);
+}
 
-            const infoBox2 = document.createElement('div')
-            infoBox2.className = 'info-box'
-            weatherRow.append(infoBox2)
+function createImageElement(weatherPicture, infoBox) {
+    const imgElement = document.createElement('img')
+    imgElement.src = weatherPicture
+    imgElement.className = 'info-box-img'
+    infoBox.append(imgElement)
+}
 
-            const imgWork = document.createElement('img')
-            imgWork.src = weatherForecast.workWeatherPicture
-            imgWork.className = 'info-box-img'
-            infoBox2.append(imgWork)
+function createInfoBoxElement(location, temperature, feelsLikeTemp, infoBox) {
+    const contentInfoBox = document.createElement('ul')
+    contentInfoBox.className = 'info-box-content'
 
-        }
-    ).catch(error => {
-        console.log(error)
-        console.log('Something somewhere went wrong. What could it be..?')
-    })
+    const locationElement = document.createElement('li')
+    locationElement.textContent = location
+    contentInfoBox.append(locationElement)
 
+    const temperatureElement = document.createElement('li')
+    temperatureElement.textContent = "Temperature: " + temperature
+    contentInfoBox.append(temperatureElement)
+
+    const feelsLikeTempElement = document.createElement('li')
+    feelsLikeTempElement.textContent = "Temperature feels like: " + feelsLikeTemp
+    contentInfoBox.append(feelsLikeTempElement)
+
+    infoBox.append(contentInfoBox)
 }
 
