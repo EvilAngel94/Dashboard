905747dd9d4691b889e589a4a1cdc8dee17c7055
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-08-17 19:20:18 +0200

    Weatherforecast - Refactored some JS files

---
+++ src/main/resources/static/js/weather/weather.js
@@ -0,0 +1,102 @@
+var isAlreadyExecuted = false
+
+/**
+ * This function is responsible for displaying the information obtained from the server regarding weather forecast
+ */
+function getWeatherForecast() {
+    if (!isAlreadyExecuted) {
+        fetch('http://localhost:8080/rest/weather')
+            .then(response => {
+                return response.json()
+            }).then(
+            data => {
+                const weatherRow = document.getElementById("row-details");
+                weatherRow.className = "row-detail";
+
+                const weatherForecast = new WeatherForecast(data.weatherForecast, data.homeWeatherPicture, data.workWeatherPicture);
+
+                // Creation of the home element
+                createInfoBox(
+                    weatherRow,
+                    weatherForecast.homePictureLocation,
+                    weatherForecast.homeLocation,
+                    weatherForecast.homeTemp,
+                    weatherForecast.homeFeelsLikeTemp
+                );
+
+                // Creation of the work element
+                createInfoBox(
+                    weatherRow,
+                    weatherForecast.workPictureLocation,
+                    weatherForecast.workLocation,
+                    weatherForecast.workTemp,
+                    weatherForecast.workFeelsLikeTemp
+                );
+                isAlreadyExecuted = true;
+            }
+        ).catch(error => {
+            console.log(error);
+            console.log('Something somewhere went wrong. What could it be...?');
+        })
+    } else {
+        document.getElementById('row-details').remove();
+        isAlreadyExecuted = false;
+    }
+}
+
+class WeatherForecast {
+    /**
+     * @param forecast.homeLocation: String
+     * @param forecast.workLocation: String
+     * @param forecast.homeTemperature: Double
+     * @param forecast.homeTemperatureFeelsLike: Double
+     * @param forecast.workTemperature: Double
+     * @param forecast.workTemperatureFeelsLike: Double
+     */
+    constructor(forecast, homeLocationPicture, workLocationPicture){
+        this.homePictureLocation = homeLocationPicture;
+        this.workPictureLocation = workLocationPicture;
+        this.homeLocation = forecast.homeLocation;
+        this.homeTemp = forecast.homeTemperature;
+        this.homeFeelsLikeTemp = forecast.homeTemperatureFeelsLike;
+        this.workLocation = forecast.workLocation;
+        this.workTemp = forecast.workTemperature;
+        this.workFeelsLikeTemp = forecast.workTemperatureFeelsLike;
+    };
+}
+
+function createInfoBox(weatherRow, weatherPicture, location, temperature, feelsLikeTemp) {
+    const infoBox = document.createElement('div');
+    infoBox.className = 'info-box';
+    weatherRow.append(infoBox);
+
+    createImageElement(weatherPicture, infoBox);
+    createInfoBoxElement(location, temperature, feelsLikeTemp, infoBox);
+}
+
+function createImageElement(weatherPicture, infoBox) {
+    const imgElement = document.createElement('img');
+    imgElement.src = weatherPicture;
+    imgElement.className = 'info-box-img';
+    infoBox.append(imgElement);
+}
+
+function createInfoBoxElement(location, temperature, feelsLikeTemp, infoBox) {
+    const contentInfoBox = document.createElement('ul');
+    contentInfoBox.className = 'info-box-content';
+
+    const locationElement = document.createElement('li');
+    locationElement.textContent = location;
+    contentInfoBox.append(locationElement);
+
+    const temperatureElement = document.createElement('li');
+    temperatureElement.textContent = "Temperature: " + temperature;
+    contentInfoBox.append(temperatureElement);
+
+    const feelsLikeTempElement = document.createElement('li');
+    feelsLikeTempElement.textContent = "Temperature feels like: " + feelsLikeTemp;
+    contentInfoBox.append(feelsLikeTempElement);
+
+    infoBox.append(contentInfoBox);
+}
+
