65293327803a42d82093ab0b4c5969f1243f1418
Michiel de Smet <Michieldesmet1994@gmail.com>
2020-09-06 21:59:59 +0200

    Vue - WIP it is rendering but should be revisionned

--- frontend/src/components/InteractablesApp.vue
+++ frontend/src/components/InteractablesApp.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="row">
-    <!-- https://stackoverflow.com/questions/54408841/vue-js-insert-block-for-every-6th-loop-element -->
-    <Interactable
+  <!--  <div class="row">-->
+  <!--    &lt;!&ndash; https://stackoverflow.com/questions/54408841/vue-js-insert-block-for-evvery-6th-loop-element &ndash;&gt;-->
+  <!--    <Interactable-->
+  <!--            v-for="interactable in interactables"-->
+  <!--            :key="interactable.id"-->
+  <!--            :Interactable="interactable"-->
+  <!--    />-->
+  <!--  </div>-->
+  <div id="app">
+    <div class="row" :key="i" v-for="i in Math.ceil(interactables.length / 3)">
+      <div :key="offer" v-for="offer in interactables.slice((i-1)*3, i*3)">
+        <Interactable
             v-for="interactable in interactables"
             :key="interactable.id"
-            :Interactable="interactable"
-    />
+            :Interactable="offer"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,35 +25,35 @@ import Interactable from "./Interactable";
 import InteractableService from "@/service/InteractableService";
 
 export default {
-    name: "InteractablesApp",
-    components: {
-      Interactable
-    },
-    data() {
-      return {
-        interactables: []
-      }
-    },
-    methods: {
-      allButtons() {
-        InteractableService.getAllInteractables()
+  name: "InteractablesApp",
+  components: {
+    Interactable
+  },
+  data() {
+    return {
+      interactables: []
+    }
+  },
+  methods: {
+    allButtons() {
+      InteractableService.getAllInteractables()
           .then(response => {
             this.interactables = response.data;
           });
-      }
-    },
-    created() {
-      this.allButtons();
     }
+  },
+  created() {
+    this.allButtons();
   }
+}
 </script>
 
 <style scoped>
-  .row {
-    border-radius: 8px;
-    display: flex;
-    height: 220px;
-    justify-content: space-evenly;
-    margin: 10px;
-  }
+.row {
+  border-radius: 8px;
+  display: flex;
+  height: 220px;
+  justify-content: space-evenly;
+  margin: 10px;
+}
 </style>
