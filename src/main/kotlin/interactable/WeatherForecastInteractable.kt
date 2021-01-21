eacbf8fd2193852035c59935303294552b8974f8
Michiel de Smet <Michieldesmet1994@gmail.com>
2021-01-21 15:54:34 +0100

    #63 - WIP

--- src/main/kotlin/interactable/WeatherForecastInteractable.kt
+++ src/main/kotlin/interactable/WeatherForecastInteractable.kt
@@ -1,15 +1,15 @@
-package interactable
-
-import interactable.weather.WeatherForecast
-
-/**
- * This interface is responsible for obtaining the weather forecast
- */
-interface WeatherForecastInteractable {
-
-    /**
-     * Retrieve the weather forecast of the home location and work location
-     */
-    fun getWeatherForecast(home: String, work: String, apiKey: String) : WeatherForecast
-
-}
+//package interactable
+//
+//import interactable.weather.WeatherForecast
+//
+///**
+// * This interface is responsible for obtaining the weather forecast
+// */
+//interface WeatherForecastInteractable {
+//
+//    /**
+//     * Retrieve the weather forecast of the home location and work location
+//     */
+//    fun getWeatherForecast(home: String, work: String, apiKey: String) : WeatherForecast
+//
+//}
--- src/main/kotlin/interactable/weather/CityAndId.kt
+++ src/main/kotlin/interactable/weather/CityAndId.kt
@@ -1,25 +1,25 @@
-package interactable.weather
-
-/**
- * This Enum is responsible for co-relating a city with their ID. This is based on the WeatherApi data.
- */
-enum class CityAndId(val id: Int) {
-
-    Zaamslag(2744122),
-    Merelbeke(2791315);
-
-    companion object {
-        /**
-         * This method searches for the city name, and returns their id.
-         * If nothing is found, returns null.
-         */
-        fun convertCityToId(location: String): String {
-            val foundValue = values().find {
-                it.name == location
-            }
-            return foundValue?.id.toString();
-        }
-    }
-
-}
-
+//package interactable.weather
+//
+///**
+// * This Enum is responsible for co-relating a city with their ID. This is based on the WeatherApi data.
+// */
+//enum class CityAndId(val id: Int) {
+//
+//    Zaamslag(2744122),
+//    Merelbeke(2791315);
+//
+//    companion object {
+//        /**
+//         * This method searches for the city name, and returns their id.
+//         * If nothing is found, returns null.
+//         */
+//        fun convertCityToId(location: String): String {
+//            val foundValue = values().find {
+//                it.name == location
+//            }
+//            return foundValue?.id.toString();
+//        }
+//    }
+//
+//}
+//
