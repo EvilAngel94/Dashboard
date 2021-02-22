2aa26f958e23c1228e0fb870395799e06459274a
Michiel de Smet <michiel.desmet@axians.com>
2021-02-22 17:59:31 +0100

    docker - first setup but not working

---
+++ docker/pg-init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Interactable (
+    id int PRIMARY KEY,
+    redirectionUrl VARCHAR(255),
+    placeholderSrc VARCHAR(255),
+    src VARCHAR(255),
+    slug VARCHAR(255),
+    gif VARCHAR(255),
+    title VARCHAR(255)
+)
